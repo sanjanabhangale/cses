9e98a132532340c0483f7d07ee99bfb5a51fbc4c
Sanjana Bhangale <bsanjana@athenahealth.com>
2023-09-14 21:49:22 +0530

    weird algorithm updated/improved

--- Weird Algorithm/weird_algo.py
+++ Weird Algorithm/weird_algo.py
@@ -1,16 +1,24 @@
-def main():
-    n = int(input())
-    weirdalgo(n)
-
-def weirdalgo(n):
+n = int(input())
+while n!=1:
     print(n)
-    if n==1:
-        return
+    if n%2==0:
+        n = n//2
     else:
-        if(int(n)%2==0):
-            weirdalgo(int(n/2))
-        else:
-            weirdalgo(int(n)*3+1)
+        n = n * 3 +1
+print(n)
 
-if __name__ == "__main__":
-    main()
+# def main():
+#     n = int(input())
+#     weirdalgo(n)
+
+# def weirdalgo(n):
+#     print(n)
+#     if n==1:
+#         return
+#     elif(int(n)%2==0):
+#         weirdalgo(int(n/2))
+#     else:
+#         weirdalgo(int(n)*3+1)
+
+# if __name__ == "__main__":
+#     main()
